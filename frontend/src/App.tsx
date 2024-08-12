3946ca08156824f114e952bc17415992c73028b9
May Thu Htet <maythuhtet2892@gmail.com>
2024-08-12 10:23:06 +0100

    adding routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,24 @@
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Chat from "./components/pages/chat";
+import Home from "./components/pages/home";
+import Login from "./components/pages/login";
+import Signup from "./components/pages/Signup";
+import NotFound from "./components/pages/NotFound";
+
 function App() {
-  return <main>Hello World</main>;
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
