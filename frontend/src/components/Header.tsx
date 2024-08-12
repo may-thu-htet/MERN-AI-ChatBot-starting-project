ed65bee59b2a43b46edccc9827b30eb8d014d735
May Thu Htet <maythuhtet2892@gmail.com>
2024-08-12 12:29:24 +0100

    login and signup buttons

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,14 +2,51 @@ import React from "react";
 import { AppBar } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
 import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
+  const auth = useAuth();
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
       <Toolbar sx={{ display: "flex" }}>
         <Logo></Logo>
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                text="Go to Chat"
+                textColor="black"
+                to="/chat"
+              />
+              <NavigationLink
+                bg="#51538f"
+                text="Logout"
+                textColor="white"
+                to="/"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                text="login"
+                textColor="black"
+                to="/login"
+              />
+              <NavigationLink
+                bg="#51538f"
+                text="signup"
+                textColor="white"
+                to="/signup"
+              />
+            </>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
