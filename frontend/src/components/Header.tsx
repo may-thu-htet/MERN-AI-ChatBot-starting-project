c53f3d45c1a429ec2e7937e40a24e862c350204d
May Thu Htet <maythuhtet2892@gmail.com>
2024-08-12 11:05:56 +0100

    added logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { AppBar } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
+import Logo from "./shared/Logo";
 
 const Header = () => {
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
-      <Toolbar sx={{ display: "flex" }}></Toolbar>
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo></Logo>
+      </Toolbar>
     </AppBar>
   );
 };
