ac9c55ff6923ccd161b1e8d31bfcd24143b4fc8d
May Thu Htet <maythuhtet2892@gmail.com>
2024-08-13 10:53:16 +0100

    verification of user using cookies

--- backend/src/utils/tokens-manager.ts
+++ backend/src/utils/tokens-manager.ts
@@ -14,4 +14,19 @@ export const verifyToken = (
   next: NextFunction
 ) => {
   const token = req.signedCookies[`${COOKIE_NAME}`];
+  if (!token || token.trim() === "") {
+    return res.status(401).json({ message: "Token not received" });
+  }
+  return new Promise<void>((resolve, reject) => {
+    return jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
+      if (err) {
+        reject(err.message);
+        return res.status(401).json({ message: "Token expired" });
+      } else {
+        resolve();
+        res.locals.jwtData = success;
+        return next();
+      }
+    });
+  });
 };
