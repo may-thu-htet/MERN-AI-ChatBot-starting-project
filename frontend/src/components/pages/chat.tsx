221ab9ea54c5fe59d37c4c0a82a1b9c9d1306c5f
May Thu Htet <maythuhtet2892@gmail.com>
2024-08-13 15:05:04 +0100

    UI of chats

--- frontend/src/components/pages/chat.tsx
+++ frontend/src/components/pages/chat.tsx
@@ -1,10 +1,113 @@
+import { Avatar, Box, Typography, Button } from "@mui/material";
+import red from "@mui/material/colors/red";
 import React from "react";
+import { useAuth } from "../../context/AuthContext";
 
 const Chat = () => {
+  const auth = useAuth();
   return (
-    <div>
-      <h1>Chat</h1>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box
+        sx={{
+          display: { md: "flex", xs: "none", sm: "none" },
+          flex: 0.2,
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "60vh",
+            bgcolor: "rgb(17,29,39)",
+            borderRadius: 5,
+            flexDirection: "column",
+            mx: 3,
+          }}
+        >
+          <Avatar
+            sx={{
+              m: "auto",
+              my: 2,
+              bgcolor: "white",
+              color: "black",
+              fontWeight: 700,
+            }}
+          >
+            {auth?.user?.name[0]}
+            {auth?.user?.name.split(" ")[1][0]}
+          </Avatar>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+            You are talking to a chat bot.
+          </Typography>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans", my: 4, p: 3 }}>
+            You can ask me any questions related to business, education,
+            knwoledge, advice, etc. But please advoid providing personal
+            information.
+          </Typography>
+          <Button
+            sx={{
+              width: "200px",
+              my: "auto",
+              color: "white",
+              bgcolor: red[300],
+              mx: "auto",
+              fontWeight: "700",
+              borderRadius: 3,
+              ":hover": {
+                bgcolor: red.A400,
+              },
+            }}
+          >
+            Clear Conversation
+          </Button>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flex: { md: 0.8, sm: 1, xs: 1 },
+          flexDirection: "column",
+          px: 3,
+        }}
+      >
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontSize: "40px",
+            color: "white",
+            mb: 2,
+            mx: "auto",
+            fontWeight: "600",
+          }}
+        >
+          Model GPT - 3.5 Turbo
+        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+            height: "60vh",
+            borderRadius: 3,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            overflow: "scroll",
+            overflowX: "hidden",
+            overflowY: "auto",
+            scrollBehavior: "smooth",
+          }}
+        ></Box>
+      </Box>
+    </Box>
   );
 };
 
