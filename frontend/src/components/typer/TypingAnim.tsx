53a612a84a4b9dd3203a61691b6dc8c387a726bd
May Thu Htet <maythuhtet2892@gmail.com>
2024-08-14 16:08:37 +0100

    type animation added

---
+++ frontend/src/components/typer/TypingAnim.tsx
@@ -0,0 +1,25 @@
+import { TypeAnimation } from "react-type-animation";
+
+export const TypeAnim = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed once, initially
+        "Chat with your OWN AI",
+        1000,
+        "Built with open AI 🤖",
+        2000,
+        "Your own customized ChatGPT 💻",
+        1500,
+      ]}
+      speed={50}
+      style={{
+        fontSize: "60px",
+        color: "white",
+        display: "inline-block",
+        boxShadow: "1px 1px 20px #000",
+      }}
+      repeat={Infinity}
+    />
+  );
+};
